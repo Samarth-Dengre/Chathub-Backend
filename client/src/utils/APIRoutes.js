4c77d356830e7fce43b9cd66ac418458872c1e15
Samarth-Dengre <samarth12911@gmail.com>
2022-11-12 19:22:22 +0530

    Changed host port in client api

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chathubreact.herokuapp.com";
+export const host = `http://localhost:${process.env.PORT}`;
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
